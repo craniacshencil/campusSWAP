594acbd65da94db3870cb55485d0455f6e0947ba
blacklytning <blacklytning@yandex.com>
2024-03-01 18:34:33 +0530

    Add forgot password ui

--- frontend/src/pages/Settings.vue
+++ frontend/src/pages/Settings.vue
@@ -6,16 +6,34 @@
             <Avatar icon = "pi pi-user" shape = "circle" size = "large" />
         </div>
     </header>
+    <div style="margin-left: 300px;">
+        <h2>Reset Password</h2>
+        <FloatLabel style="margin-top: 30px;">
+            <Password v-model="value" inputId="password" :feedback="false" />
+            <label for="password">Current Password</label>
+        </FloatLabel>
+        <FloatLabel style="margin-top: 30px;">
+            <Password v-model="value" inputId="password" />
+            <label for="password">New Password</label>
+        </FloatLabel>
+        <FloatLabel style="margin-top: 30px;">
+            <Password v-model="value" inputId="password" />
+            <label for="password">Confirm New Password</label>
+        </FloatLabel>
+
+        <Button style="margin-top: 30px;" @click = "submitForm" label = "Submit" />
+    </div>
 </template>
 
 <script>
-import InputGroup from 'primevue/inputgroup';
-import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
 import Avatar from 'primevue/avatar';
+import Password from 'primevue/password';
+import FloatLabel from 'primevue/floatlabel';
+
 
 export default{
-    components: { Avatar, InputText, Button, InputGroup }
+    components: { Avatar, Button, Password, FloatLabel }
 }
 </script>
 
