e6de6029ce5303faf1153e480dee1a763d3a092a
blacklytning <blacklytning@yandex.com>
2024-03-29 12:34:11 +0530

    Add url for all_listings

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path("image_url_gen", views.generate_image_url, name = "image_url_gen"),
     path("sell_form", views.sell_form, name = "sell_form"),
     path("user_listings/<int:moodleID>", views.user_listings, name = "user_listings"),
+    path("all_listings", views.all_listings, name = "all_listings"),
 ] 
